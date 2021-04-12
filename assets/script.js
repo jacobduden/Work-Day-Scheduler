d1ae64fc6c98a0d8fc32ca3c8cef7df6d2c83a02
jacobduden <75395688+jacobduden@users.noreply.github.com>
2021-04-11 21:09:05 -0500

    got local storage on page

--- assets/script.js
+++ assets/script.js
@@ -3,19 +3,21 @@ $("#currentDay").html(todaysDate);
 
 $(document).ready(function() {
     // Save Button click event
-    $("#saveBtn").on("click", function() {
+    $(".saveBtn").on("click", function() {
         
-        var text = $(this).sibblings(".description").val();
-        var time = $(this).parent().attr("id")
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
         //Saves the text to local storage
-        localStorage.setItem(text, time);
+        localStorage.setItem(time, text);
     })
-})
+
 
 function trackCurrentTime() {
+    //fetches the current time
     var currentTime = moment().hour();
-
+console.log(currentTime);
+    //loops over each block and changes background color based on the currentTime
     $(".time-block").each(function(){
         var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
 
@@ -36,4 +38,17 @@ function trackCurrentTime() {
         }
     })
 }
+//gets items saved in local storage
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
+trackCurrentTime();
+
+})
