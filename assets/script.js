854bf4a7084ff00542e191ddb550e9945dfc9193
jacobduden <75395688+jacobduden@users.noreply.github.com>
2021-04-11 21:30:01 -0500

    Fixed typos

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 var todaysDate = moment().format('dddd, MMM Do YYYY')
 $("#currentDay").html(todaysDate);
 
-$(document).ready(function() {
+$(document).ready(function () {
+
     // Save Button click event
-    $(".saveBtn").on("click", function() {
-        
+    $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
@@ -13,42 +13,43 @@ $(document).ready(function() {
     })
 
 
-function trackCurrentTime() {
-    //fetches the current time
-    var currentTime = moment().hour();
-console.log(currentTime);
-    //loops over each block and changes background color based on the currentTime
-    $(".time-block").each(function(){
-        var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+    function trackCurrentTime() {
 
-        if(timeBlock < currentTime) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        }
-        else if (timeBlock === currentTime){
-            $(this).removeClass("future");
-            $(this).removeClass("past");
-            $(this).addClass("present");
-        }
-        else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future")
-        }
-    })
-}
-//gets items saved in local storage
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+        //fetches the current time
+        var currentTime = moment().hour();
+        
+        //loops over each block and changes background color based on the currentTime
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future")
+            }
+        })
+    }
 
-trackCurrentTime();
+    //gets items saved in local storage
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    trackCurrentTime();
 
 })
